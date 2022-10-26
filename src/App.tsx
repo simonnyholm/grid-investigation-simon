bd562a63635cd2ba6630554b08552c54b8f5d6fe
Gert Rue Brigsted <gpb@kmd.dk>
2022-10-26 10:21:47 +0200

    Added basic data handling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import data from './data.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      { data.map((group) => (
+          <h1>{ group.amount }</h1>
+      ))}
+    </Container>
   );
 }
 
