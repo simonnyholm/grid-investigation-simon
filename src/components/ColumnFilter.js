7d0eedb326fb59f77898b7710935d13e96cd0e83
Simon Nyholm Sørensen <simonnyholm@hotmail.com>
2022-11-07 14:56:17 +0100

    add columnFilter component

---
+++ src/components/ColumnFilter.js
@@ -0,0 +1,13 @@
+
+
+const ColumnFilter = ({ column }) => {
+    const { filterValue, setFilter } = column
+    return (
+        <span>
+            Search:{' '}
+            <input type="text" value={filterValue || ''} onChange={(e) => setFilter(e.target.value)}/>
+        </span>
+    )
+}
+
+export default ColumnFilter;
