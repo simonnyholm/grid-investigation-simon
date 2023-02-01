ecd58dc0d131ac281161f5f55b991f22fd9e7c0d
Simon Nyholm Sørensen <simonnyholm@hotmail.com>
2023-02-01 09:40:45 +0100

    edit show only 2 fractions

--- src/components/utils/formatCurrency.test.js
+++ src/components/utils/formatCurrency.test.js
@@ -2,15 +2,15 @@ import formatCurrency from "./formatCurrency";
 
 describe('formatCurrency', () => {
     it('formats numbers with two fractional digits', () => {
-        expect(formatCurrency({value: 10 })).toBe('10.000');
+        expect(formatCurrency({value: 10 })).toBe('10.00');
     });
 
     it('always have at least one integer digit', () => {
-        expect(formatCurrency({ value: 0.25 })).toBe('0.250');
+        expect(formatCurrency({ value: 0.25 })).toBe('0.25');
     });
 
     it('rounds numbers to nearest two fractional digits', () => {
-        expect(formatCurrency({ value: 0.2224 })).toBe('0.222');
-        expect(formatCurrency({ value: 0.2225 })).toBe('0.223');
+        expect(formatCurrency({ value: 0.224 })).toBe('0.22');
+        expect(formatCurrency({ value: 0.225 })).toBe('0.23');
     });
 });
