0e08370406e73eb0b84639b7553eb2ec53578c44
Simon Nyholm Sørensen <simonnyholm@hotmail.com>
2023-02-03 10:37:37 +0100

    no imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import { COLUMNS } from './components/Columns.jsx'
+
+import dataJson from './data.json'
 
 import GenericTable from './components/GenericTable';
+
+const flatData = dataJson.flatMap((transactionGroup) => transactionGroup.transactions);
 
 
 function App() {
   return (
     <div style={{padding: "15px"}}>
 
-    <GenericTable/>
+    <GenericTable data={flatData} columns={COLUMNS} groupBy={['']}/>
     </div>
   );
 }
