5dd565e113261cd3381c53affa66bb175240d8e1
Gert Rue Brigsted <gpb@kmd.dk>
2023-02-09 09:02:12 +0100

    Fixed formatting test.

--- src/components/utcFormat.js
+++ src/components/utcFormat.js
@@ -1,12 +1,7 @@
 import { DateTime } from "luxon";
 
 export default function utcFormat({ value }){
-
     const utcFormatConst = new Intl.DateTimeFormat('da', {day: 'numeric', month: 'short', year: 'numeric'});
-
     const exportTimeVar = utcFormatConst.format(new Date(value))
-
-    console.log("exportTimeVar", exportTimeVar);
-
     return exportTimeVar
 }
